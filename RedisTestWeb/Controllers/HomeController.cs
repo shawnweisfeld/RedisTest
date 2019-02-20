13097ee8a18f817a00225368557bd020e1b629aa
Shawn Weisfeld <shawn@shawnweisfeld.com>
2019-02-20 14:35:43 -0600

    Initial commit

---
+++ RedisTestWeb/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedisTestWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            IDatabase cache = Connection.GetDatabase();
+
+            await cache.StringSetAsync("myvalue", DateTime.UtcNow.ToLongTimeString());
+
+            return View();
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IDatabase cache = Connection.GetDatabase();
+
+            ViewBag.Message = await cache.StringGetAsync("myvalue");
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}

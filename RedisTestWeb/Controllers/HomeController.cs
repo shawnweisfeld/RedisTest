6fbf2ebfa15b8de8a807a5900850fb600b711f9f
Shawn Weisfeld <shawn@shawnweisfeld.com>
2019-02-21 19:23:57 -0600

    add dependency tracking for redis in AI

--- RedisTestWeb/Controllers/HomeController.cs
+++ RedisTestWeb/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
-﻿using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
+﻿using System;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace RedisTestWeb.Controllers
@@ -13,32 +8,14 @@ namespace RedisTestWeb.Controllers
     {
         public async Task<ActionResult> Index()
         {
-            IDatabase cache = Connection.GetDatabase();
-
-            await cache.StringSetAsync("myvalue", DateTime.UtcNow.ToLongTimeString());
+            await RedisHelper.StringSetAsync("myvalue", DateTime.UtcNow.ToLongTimeString());
 
             return View();
         }
 
-        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
-        {
-            string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
-            return ConnectionMultiplexer.Connect(cacheConnection);
-        });
-
-        public static ConnectionMultiplexer Connection
-        {
-            get
-            {
-                return lazyConnection.Value;
-            }
-        }
-
         public async Task<ActionResult> About()
         {
-            IDatabase cache = Connection.GetDatabase();
-
-            ViewBag.Message = await cache.StringGetAsync("myvalue");
+            ViewBag.Message = await RedisHelper.StringGetAsync("myvalue");
 
             return View();
         }
---
+++ RedisTestWeb/RedisHelper.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using StackExchange.Redis;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace RedisTestWeb
+{
+    public static class RedisHelper
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        private static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+
+        private static Lazy<TelemetryClient> lazyTelemetry = new Lazy<TelemetryClient>(() =>
+        {
+            return new TelemetryClient();
+        });
+
+        private static TelemetryClient Telemetry
+        {
+            get
+            {
+                return lazyTelemetry.Value;
+            }
+        }
+
+        public static async Task<bool> StringSetAsync(string key, string value)
+        {
+            var dependency = new DependencyTelemetry()
+            {
+                Type = "Redis",
+                Name = "StringSetAsync"
+            };
+            dependency.Properties["key"] = key;
+
+            using (Telemetry.StartOperation(dependency))
+            {
+                var result = await Connection.GetDatabase().StringSetAsync(key, value);
+                dependency.Success = result;
+                return result;
+            }
+        }
+
+        public static async Task<RedisValue> StringGetAsync(string key)
+        {
+            var dependency = new DependencyTelemetry()
+            {
+                Type = "Redis",
+                Name = "StringGetAsync"
+            };
+            dependency.Properties["key"] = key;
+
+            using (Telemetry.StartOperation(dependency))
+            {
+                var result = await Connection.GetDatabase().StringGetAsync(key);
+                dependency.Success = true;
+                return result;
+            }
+        }
+    }
+}
